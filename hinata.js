5303bb9c1e9aed5c5174faa1b26c6e7c13cbdbb4
zhih <fufong79570@gmail.com>
2021-05-09 15:31:57 +0800

    modifing

--- hinata.js
+++ hinata.js
@@ -28,6 +28,33 @@ var questions = [
   }
 ];
 
+// 產生ODM
+const produceDoc = (body) => {
+  return new jsdom.JSDOM(body).window.document;
+};
+// 確認資料夾存在
+const notExist = (path) => {
+  return new Promise((res, rej) => {
+    fs.access(path, (err) => {
+      if (err) {
+        return res(true);
+      } else {
+        return rej(false);
+      }
+    });
+  });
+};
+
+//參數設定
+const params = (res) => {
+  const document = produceDoc(res.data);
+  const name = document.querySelector('.c-blog-article__name').textContent.trim();
+  const title = document.querySelectorAll(".c-blog-article__title");
+  const date = document.querySelectorAll('.c-blog-article__date');
+  const content = document.querySelectorAll('.c-blog-article__text');
+  return [name, title, date, content];
+};
+
 inquirer.prompt(questions).then(res => {
   MEMBER_NUM = +res.num;
   page = +res.page - 1;
@@ -36,44 +63,50 @@ inquirer.prompt(questions).then(res => {
     pageUrl.push(
       `https://www.hinatazaka46.com/s/official/diary/member/list?page=${i}&ct=${MEMBER_NUM}`)
   };
+  return pageUrl
+}).then(pageUrl =>{
+  return Promise.all(pageUrl.map((page) => axios.get(page)))
+}).then(blogsPerPage =>{
+   blogsPerPage.map(blogs =>{
+      const [name, title, date,  content]= params(blogs);
+      return [name, title, date,  content]
+  })
+  
+})
 
-  pageUrl.forEach(page => {
-    axios.get(page).then(res => {
-      const dom = new jsdom.JSDOM(res.data);
-      const document = dom.window.document;
-      const name = document.querySelector('.c-blog-article__name').textContent.trim();
-      const title = document.querySelectorAll(".c-blog-article__title");
-      const date = document.querySelectorAll('.c-blog-article__date');
-      const content = document.querySelectorAll('.c-blog-article__text');
-      title.forEach((item, i) => {
+  // pageUrl.forEach(page => {
+  //   axios.get(page).then(res => {
+  //     const [name, title, date,  content]= params(res);
 
-        let ym = date[i].textContent.trim().replace(":", "時").replace(' ', '-');
+  //     title.forEach((item, i) => {
 
-        let newTitle = title[i].textContent.trim()
+  //       let ym = date[i].textContent.trim().replace(":", "時").replace(' ', '-');
 
-        const path = `${__dirname}/${name}/${ym}`
+  //       let newTitle = title[i].textContent.trim()
 
-        mkdirp(path).then(made =>
-          console.log(`建立資料夾成功`)).then(() => {
-            //blog content
-            fs.writeFile(`${path}/${newTitle.slice(0,10)}.html`, title[i].innerHTML+'<br><br><br>'+content[i].innerHTML, function (err, result) {
-              if (err) console.log('error', err);
-              console.log('文章下載成功');
-            });
-            // download pics
-            const blogPicAll = content[i].querySelectorAll('img')
-            let pics = [...blogPicAll].map(pic => pic.src)
-            pics.forEach(pic => {
-              download(pic, path).then(() => console.log('我看者妳載照片 嘿嘿'))
+  //       const path = `${__dirname}/${name}/${ym}`
 
-            })
-          });
-      });
+  //       mkdirp(path).then(made =>
+  //         console.log(`建立資料夾成功`)).then(() => {
+  //           //blog content
+  //           fs.writeFile(`${path}/${newTitle.slice(0,10)}.html`, title[i].innerHTML+'<br><br><br>'+content[i].innerHTML, function (err, result) {
+  //             if (err) console.log('error', err);
+  //             console.log('文章下載成功');
+  //           });
+  //           // download pics
+  //           const blogPicAll = content[i].querySelectorAll('img')
+  //           let pics = [...blogPicAll].map(pic => pic.src)
+  //           pics.forEach(pic => {
+  //             download(pic, path).then(() => console.log('我看者妳載照片 嘿嘿'))
+
+  //           })
+  //         });
+  //     });
 
 
-    });
+  //   });
 
-  });
+  // });
 
 
-});
+
