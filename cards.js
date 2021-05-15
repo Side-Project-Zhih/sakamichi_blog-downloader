c28d92950d5d750418fe636ea9788a02d4c624f9
zhih <fufong79570@gmail.com>
2021-05-15 19:53:13 +0800

    Create cards folder

--- cards.js
+++ cards.js
@@ -1,53 +1,31 @@
 const fs = require("fs");
-// const https = require("https");
-
-// const mkdirp = require("mkdirp");
 const download = require("download");
 const axios = require("axios");
 const jsdom = require("jsdom");
-
+let data = require("./member.json");
 const produceDoc = (body) => {
   return new jsdom.JSDOM(body).window.document;
 };
 
-const readFile = (fileName) => {
-  return new Promise((res, rej) => {
-    fs.readFile(fileName, (err, data) => {
-      if (err) return rej(err);
-      return res(data);
-    });
-  });
-};
+let membersUrl =data.map(
+  (member) => `https://sakurazaka46.com/s/s46/artist/${member}`
+);
 
-readFile("member.json")
-  .then(
-    (data) => {
-      let membersUrl = [...JSON.parse(data)].map(
-        (member) => `https://sakurazaka46.com/s/s46/artist/${member}`
-      );
-      // console.log(membersUrl)
-      return membersUrl;
-    },
-    (err) => console.log(err)
-  )
-  .then((membersUrl) => {
-    return Promise.all(
-      membersUrl.map(async (member) => {
-        let body = await axios.get(member);
-        body = body.data;
-        const document = produceDoc(body);
-        const name = document.querySelector(".prof-elem .name").textContent;
-        let pics = document.querySelectorAll(".inner-v2 img");
-        pics = [...pics];
-        pics = pics.map((item) => {
-          item.src = item.src.replace("1000_1000_102400", "");
-          return `https://sakurazaka46.com/${item.src}`;
-        });
-        return { name, pics };
-      })
-    );
+Promise.all(
+  membersUrl.map(async (member) => {
+    let body = await axios.get(member);
+    body = body.data;
+    const document = produceDoc(body);
+    const name = document.querySelector(".prof-elem .name").textContent;
+    let pics = document.querySelectorAll(".inner-v2 img");
+    pics = [...pics];
+    pics = pics.map((item) => {
+      item.src = item.src.replace("1000_1000_102400", "");
+      return `https://sakurazaka46.com/${item.src}`;
+    });
+    return { name, pics };
   })
-  .then((data) => {
+).then((data) => {
     return Promise.all(
       data.map((member) => {
         const { name, pics } = member;
