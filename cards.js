49810b5eb8281f8ccbc4a2c585a42e1a93364622
zhih <fufong79570@gmail.com>
2021-05-15 17:41:52 +0800

    Package js file to exe file

--- cards.js
+++ cards.js
@@ -5,9 +5,6 @@ const fs = require("fs");
 const download = require("download");
 const axios = require("axios");
 const jsdom = require("jsdom");
-// const inquirer = require("inquirer");
-// const totalPic = [];
-// https://sakurazaka46.com/s/s46/contents/B202010_49?ima=4513&m=49
 
 const produceDoc = (body) => {
   return new jsdom.JSDOM(body).window.document;
@@ -65,40 +62,3 @@ readFile("member.json")
   })
   .then(() => console.log("finish"))
   .catch((e) => console.log(e));
-
-//the other method
-
-// fs.readFile('member.json', function (err, members) {
-//     if (err) throw err;
-//     let membersUrl = [...JSON.parse(members)].map( member => `https://sakurazaka46.com/s/s46/artist/${member}`)
-//     // console.log(membersUrl);
-//     membersUrl.forEach( member => {
-//       axios.get(member).then (res =>{
-//         const dom  = new jsdom.JSDOM(res.data);
-//         const document = dom.window.document;
-//         const name = document.querySelector('.prof-elem .name').textContent;
-//         // console.log(name);
-//         let pics = document.querySelectorAll('.inner-v2 img');
-//         pics = [...pics]
-//         pics=pics.map(item =>{
-//           item.src=item.src.replace('1000_1000_102400','');
-//           return `https://sakurazaka46.com/${item.src}`
-//         });
-//         // console.log(pics)
-//         pics.forEach( (pic,i) =>{
-
-//               https.get(pic,(res) => {
-//               // Image will be stored at this path
-//               const path = `${__dirname}/賀卡/${name+i}.jpeg`;
-//               const filePath = fs.createWriteStream(path);
-//               res.pipe(filePath);
-//               filePath.on('finish',() => {
-//                   filePath.close();
-//                   console.log('Download Completed');
-//               })
-//           })
-
-//         });
-//       })
-//     });
-// });
